7773151b9548670991557f984254652618d2bd09
Jason <leon___bek@mail.ru>
2020-01-31 16:58:20 +0500

    Optimized

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -7,6 +7,33 @@ let result = document.querySelector('#resultId');
 const container = document.getElementById("jsoneditor");
 const options = {mode: "code"};
 const editor = new JSONEditor(container, options);
+const globalMap = {};
+
+function htmlSpecialChars(str) {
+    let map = {
+        "&": "&amp;",
+        "<": "&lt;",
+        ">": "&gt;",
+        "\"": "&quot;",
+        "'": "&#39;" // ' -> &apos; for XML only
+    };
+    return str.replace(/[&<>"']/g, function (m) {
+        return map[m];
+    });
+}
+
+function htmlSpecialCharsDecode(str) {
+    let map = {
+        "&amp;": "&",
+        "&lt;": "<",
+        "&gt;": ">",
+        "&quot;": "\"",
+        "&#39;": "'"
+    };
+    return str.replace(/(&amp;|&lt;|&gt;|&quot;|&#39;)/g, function (m) {
+        return map[m];
+    });
+}
 
 // set json
 const initialJson = {
@@ -34,7 +61,30 @@ const initialJson = {
         "mapping": "greeting"
     }
 };
-editor.set(initialJson)
+editor.set(initialJson);
+
+function copyCode(id) {
+    /* let copyText = document.getElementById(id);
+     copyText.select();
+     document.execCommand("copy");
+     alert("Copied the text: " + copyText.value);*/
+
+    const el = document.createElement('textarea');
+    el.value = globalMap[id];
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    alert("Copied the text: " + el.value);
+}
+
+function generateCodeBlock(id, text) {
+    globalMap[id] = text;
+    return `<pre><button onclick="copyCode('${id}')">copy</button><code>${htmlSpecialChars(text)}</code></pre>`;
+}
 
 function uploadSingleFile() {
     let xhr = new XMLHttpRequest();
@@ -47,11 +97,11 @@ function uploadSingleFile() {
             let {pojo, mapper, controller, service, serviceImpl} = response;
 
             let resultText = '';
-            if (pojo) resultText += `<pre><code>${pojo}</code></pre>`;
-            if (mapper) resultText += `<pre><code>${mapper}</code></pre>`;
-            if (controller) resultText += `<pre><code>${controller}</code></pre>`;
-            if (service) resultText += `<pre><code>${service}</code></pre>`;
-            if (serviceImpl) resultText += `<pre><code>${serviceImpl}</code></pre>`;
+            if (pojo) resultText += generateCodeBlock('pojoCode', pojo);
+            if (mapper) resultText += generateCodeBlock('mapperCode', mapper);
+            if (controller) resultText += generateCodeBlock('controllerCode', controller);
+            if (service) resultText += generateCodeBlock('serviceCode', service);
+            if (serviceImpl) resultText += generateCodeBlock('serviceImplCode', serviceImpl);
             result.innerHTML = resultText;
         } else {
             console.error(xhr)
